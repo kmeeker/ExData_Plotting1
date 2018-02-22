8f1612ce479b57f1d75906c6cd44509d149194b7
kmeeker <kmeeker123@gmail.com>
2018-02-22 00:24:18 -0800

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ df$Date <- as.Date(df$Date,format="%d/%m/%Y")
 df <- df[which(df$Date >= as.Date("2007-02-01") & df$Date <= as.Date("2007-02-02")),]
 df$Global_active_power <- as.numeric(df$Global_active_power)
 
-png(file="plot1.png",width=480,height=480)
-hist(df$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png(file="plot2.png",width=480,height=480)
+plot(df$DateTime,df$Global_active_power, type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
